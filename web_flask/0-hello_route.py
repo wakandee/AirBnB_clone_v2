94c272d37b9e43b199cfe7e2bc25f01c7fb8bd8b
wakande <koechallanhi@gmail.com>
2024-07-27 09:14:27 -0700

    1-hbnb_route.py HBNB

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,8 +9,9 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
-    """returns Hello HBNB!"""
-    return 'Hello HBNB!'
+        """returns Hello HBNB!"""
+            return 'Hello HBNB!'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+        if __name__ == '__main__':
+                app.run(host='0.0.0.0', port='5000')
+

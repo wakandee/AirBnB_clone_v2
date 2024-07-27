35445e3f13d13bc7bcb1a73c7a8cba34dde08b3d
wakandee <koechallanhi@gmail.com>
2024-07-27 20:46:14 +0300

    C is fun

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -5,26 +5,24 @@ start Flask application
 
 from flask import Flask
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def index():
     """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """returns HBNB"""
     return 'HBNB'
 
-@app.route('/c/<text>')
-def c_with_prams(text):
-    """returns c with params """
 
-    text_no_underscore = text.replace('_', ' ')
-    return "c {}".format(text_no_underscore)
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """display “C ” followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
